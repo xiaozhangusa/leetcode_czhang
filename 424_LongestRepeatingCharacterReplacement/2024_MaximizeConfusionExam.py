9fbe5ce2613486c2910d5d2717a5eb20c6698cf3
xiaozhangusa <adsatusa@gmail.com>
2026-01-17 05:30:40 -0500

    no need to shrking more than maxSize seen sofar

--- 424_LongestRepeatingCharacterReplacement/2024_MaximizeConfusionExam.py
+++ 424_LongestRepeatingCharacterReplacement/2024_MaximizeConfusionExam.py
@@ -37,23 +37,42 @@
 # 1 <= k <= n
 class Solution:
     def maxConsecutiveAnswers(self, answerKey: str, k: int) -> int:
-        start, end = 0, 0
+        # start, end = 0, 0
+        # freq = {'T': 0, 'F': 0}
+        # flips, res = 0, 0
+        # while end < len(answerKey):
+        #     freq[answerKey[end]] += 1
+        #     flips = min(freq['T'], freq['F'])
+        #     # print("flips: ", flips)
+        #     while flips > k and start <= end:
+        #         # print("answerKey: ", answerKey)
+        #         # print("answerKey[start]: ", start, answerKey[start])
+        #         freq[answerKey[start]] -= 1
+        #         # if freq[answerKey[start]] == 0:
+        #         #     del freq[answerKey[start]]
+        #         start += 1
+        #         flips = min(freq['T'], freq['F'])
+        #     # res = max(res, max(freq.values()))
+        #     res = max(res, (end + 1 - start))
+        #     # print("res: ", res)
+        #     end += 1
+        # return res
+        end = 0
         freq = {'T': 0, 'F': 0}
         flips, res = 0, 0
         while end < len(answerKey):
             freq[answerKey[end]] += 1
             flips = min(freq['T'], freq['F'])
-            # print("flips: ", flips)
-            while flips > k and start <= end:
-                # print("answerKey: ", answerKey)
-                # print("answerKey[start]: ", start, answerKey[start])
-                freq[answerKey[start]] -= 1
-                # if freq[answerKey[start]] == 0:
-                #     del freq[answerKey[start]]
-                start += 1
-                flips = min(freq['T'], freq['F'])
-            # res = max(res, max(freq.values()))
-            res = max(res, (end + 1 - start))
-            # print("res: ", res)
+            if flips <= k:
+                res += 1
+            else:
+                # shrink the window from the left, buy no need to keep shrinking. Invariant: the max window sofar is always valid, so we can just shrink it to the left by 1, no need to
+                # find a smaller valid window
+                # If we have already found a window of length max_size, then what we need to do next is to search for a larger valid window, for example, a window with length max_size + 1. 
+                # Therefore, in the following sliding window process, even if the current window with size max_size is not valid, there is no problem, because we have already found a window of length max_size before, so we may as well continue looking for a larger window.
+                # Why: 
+                # 1. If we have already found a window of length max_size, then what we need to do next is to search for a larger valid window, for example, a window with length max_size + 1.
+                # 2. Keep shrinking the window from the left is not a good idea because it may shrink to a smaller valid window, which is not what we want. 
+                freq[answerKey[end - res]] -= 1
             end += 1
         return res
