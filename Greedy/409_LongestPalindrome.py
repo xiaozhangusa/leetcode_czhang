faa1597170787a81b9d9e23a92685b1790b4256b
xiaozhangusa <adsatusa@gmail.com>
2026-01-25 15:45:53 -0500

    37 / 97 pass

---
+++ Greedy/409_LongestPalindrome.py
@@ -0,0 +1,34 @@
+# Given a string s which consists of lowercase or uppercase letters, return the length of the longest palindrome that can be built with those letters.
+
+# Letters are case sensitive, for example, "Aa" is not considered a palindrome.
+
+ 
+
+# Example 1:
+
+# Input: s = "abccccdd"
+# Output: 7
+# Explanation: One longest palindrome that can be built is "dccaccd", whose length is 7.
+# Example 2:
+
+# Input: s = "a"
+# Output: 1
+# Explanation: The longest palindrome that can be built is "a", whose length is 1.
+ 
+
+# Constraints:
+
+# 1 <= s.length <= 2000
+# s consists of lowercase and/or uppercase English letters only.
+
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        char_dict = Counter(s)
+        print(char_dict)
+        l, odd = 0, 0
+        for c in char_dict:
+            if char_dict[c] % 2 == 0:
+                l += char_dict[c]
+            else:
+                odd += 1
+        return l + 1 if odd > 0 else l
