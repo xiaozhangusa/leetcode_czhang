066b5d77e4d4e373fcf5235674befe213c654cae
xiaozhangusa <adsatusa@gmail.com>
2026-02-02 22:12:12 -0500

    (n + 1) not n

--- dynamic_programming/509_FibonacciNumber.py
+++ dynamic_programming/509_FibonacciNumber.py
@@ -31,8 +31,9 @@ class Solution:
     def fib(self, n: int) -> int:
         if n == 0:
             return 0
-        f = [0 for _ in range(n)]
+        f = [0 for _ in range(n + 1)]
         f[1] = 1
-        for i in range(2, n):
+        for i in range(2, n + 1):
             f[i] = f[i - 1] + f[i - 2]
-        return f[n-1]
+            # print("f[i], i: ", f[i], i)
+        return f[n]
